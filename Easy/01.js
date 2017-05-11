405c51086adc91354c1646d0f030dc5f525e4d91
McKinley Forbes <aragothofdagobah@gmail.com>
2017-05-11 02:49:18 -0500

    Flushed out Easy-01

--- Easy/01.js
+++ Easy/01.js
@@ -1,5 +1,9 @@
+//Assigning Variables
 var name = process.argv[2];
 var age = process.argv[3];
 var username = process.argv[4];
+//Assigning output Variable with other Variables
+var output = "Your name is " + name + ".\nYou are " + age + " Years old. \nYour username is " + username +".";
 
-console.log("Your name is " + name + ".\nYou are " + age + " Years old. \nYour username is " + username +".");
+//logging output
+console.log(output);
