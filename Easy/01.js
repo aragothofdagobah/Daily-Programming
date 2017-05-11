366d8d0848fd6e53e4565133e04a498290fbb08e
McKinley Forbes <aragothofdagobah@gmail.com>
2017-05-11 02:40:02 -0500

    Easy 01 Finished

---
+++ Easy/01.js
@@ -0,0 +1,5 @@
+var name = process.argv[2];
+var age = process.argv[3];
+var username = process.argv[4];
+
+console.log("Your name is " + name + ".\nYou are " + age + " Years old. \nYour username is " + username +".");
