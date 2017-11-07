95647aecdb8fd11a378c97f043226f9f3d39af45
McKinley <solidaritydesignsva@gmail.com>
2017-11-07 03:28:15 -0600

    05-filtered-ls.js

---
+++ learyounode/filtered-ls.js
@@ -0,0 +1,26 @@
+//requiring fs to read file
+const fs = require('fs');
+
+//assigning dir and file extension variables to arguments
+const dir = process.argv[2];
+const ext  = '.' + process.argv[3];
+
+
+fs.readdir(dir, (err,data) => {
+    //logs error if it exists
+    if(err){console.log(err)}
+
+    else{
+        //filters data to only allow for files that match extension
+        data = data.filter(file => {
+            if(file.substr(file.length - ext.length) === ext){
+                return file;
+            }
+        });
+
+        //log output
+        data.forEach(file => {
+            console.log(file);
+        })
+    }
+});
